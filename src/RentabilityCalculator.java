fd319ee401b324d1b8143c7172ae38886ab12808
NicolasPozzer <pozzernico@gmail.com>
2024-12-17 21:19:56 -0300

    Add: probabilidad para saber si hay cuentas de trading quemadas

--- src/RentabilityCalculator.java
+++ src/RentabilityCalculator.java
@@ -6,39 +6,59 @@ public class RentabilityCalculator {
         /* ⬇️CONFIGURAR⬇️ */
         // config estrategia
         double capitalInicial = 10000.0;
-        final double TP = 2.0; // Porcentaje de Take Profit de la cuenta
-        final double SL = 1.0; // Porcentaje de stop loss de la cuenta
+        final double TP = 2.00; // Porcentaje de Take Profit de la cuenta
+        final double SL = 1.00; // Porcentaje de stop loss de la cuenta
         final double probabilidadAciertos = 35.33; // numero entero entre 0 y 100 (Ej. 50 seria el 50% de acierto).
-        final double comisiones = 0.5;
+        final double comisiones = 0.07;
+        final double stopGestionDeRiesgo = 20.0; // si el capital inicial llega a un porcentaje menor a este se deja de operar
 
         // config pruebas
         int cantidadDeCuentasParaTestear = 10;
-        int cantidadDeTradesPorCuenta = 100;
+        int cantidadDeTradesPorCuenta = 10000;
 
         //llamado a funciones
         header();
         testearRentabilidadDeCuentas(cantidadDeCuentasParaTestear, capitalInicial,
-                cantidadDeTradesPorCuenta, probabilidadAciertos, TP, SL, comisiones);
+                cantidadDeTradesPorCuenta, probabilidadAciertos, TP, SL, comisiones, stopGestionDeRiesgo);
     }
 
 
     /* =========FUNCIONES============== */
     public static void testearRentabilidadDeCuentas(int cantidadDeCuentasParaTestear,
                                                     double capitalInicial, int cantidadDeTradesPorCuenta,
-                                                    double probabilidadAciertos, double TP, double SL, double comisiones) {
+                                                    double probabilidadAciertos, double TP, double SL, double comisiones, double stopGestionDeRiesgo) {
         double promedioEfectividadEstrategia = 0.0;
         double acumDineroFinal = 0.0;
-        //llamar a funciones
+
+        boolean detenerEjecucion = false; //por sl gestion de riesgo, osea cueta quemada
+        int cantCuentasQuedamas = 0; // cantidad de cuentas quemadas
+
+
         for (int i = 1; i <= cantidadDeCuentasParaTestear; i++) { //Recorre Cuentas
             double capitalActual = capitalInicial;
+            double porcentajeSLgestion = ((stopGestionDeRiesgo * capitalInicial) / 100); //calculo SL por Gestion
+            porcentajeSLgestion = capitalInicial - porcentajeSLgestion;
+            boolean cuentaQuemada = false;// variable local que se resetea y contamos las cuentas quemadas
 
             for (int j = 0; j < cantidadDeTradesPorCuenta; j++) { //Recorre Trades por cuenta
                 Boolean resultadoTrade = tradeRealizado(probabilidadAciertos);
+
                 if (resultadoTrade) {
                     capitalActual = capitalActual + ((TP * capitalActual) / 100);
                 }else {
                     capitalActual = capitalActual - ((SL * capitalActual) / 100);
                 }
+
+                //preguntar si el capital actual esta por debajo del stopGestionDeRiesgo
+                if(capitalActual <= porcentajeSLgestion){
+                    detenerEjecucion = true;
+                    cuentaQuemada = true;
+                }
+            }
+
+            //condicional para contar cuentas quemadas
+            if(cuentaQuemada){
+                cantCuentasQuedamas = cantCuentasQuedamas + 1;
             }
 
             //Descontar comisiones
@@ -54,10 +74,11 @@ public class RentabilityCalculator {
             acumDineroFinal = acumDineroFinal + capitalActual;
         }
 
+
         promedioEfectividadEstrategia = acumDineroFinal / cantidadDeCuentasParaTestear;
         System.out.printf("\t Promedio: $ %.2f\n",promedioEfectividadEstrategia);
         //Funcion con resultados finales de la strategia
-        resultadoFinal(promedioEfectividadEstrategia, capitalInicial, cantidadDeTradesPorCuenta);
+        resultadoFinal(promedioEfectividadEstrategia, capitalInicial, cantidadDeTradesPorCuenta, detenerEjecucion, cantCuentasQuedamas);
     }
 
     //funcion para correr bucle con pruebas de acierto
@@ -78,7 +99,8 @@ public class RentabilityCalculator {
         return tradeAcertado;
     }
 
-    public static void resultadoFinal(double promedioEfectividadEstrategia, double capitalInicial, int cantTrades){
+    public static void resultadoFinal(double promedioEfectividadEstrategia, double capitalInicial,
+                                      int cantTrades,boolean detenerEjecucion, int cantCuentasQuedamas) {
 
         //Variables para obtener el porcentaje de rentabilidad
         double sobrante = promedioEfectividadEstrategia - capitalInicial;
@@ -99,6 +121,19 @@ public class RentabilityCalculator {
             System.out.print(" Porciento de Rentabilidad Negativa en "+cantTrades+" Trades\n");
             System.out.printf("\tPromedio de Perdida: $ %.2f\n", sobrante);
         }
+
+        if(detenerEjecucion){
+            System.out.println("\n\t❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌");
+            System.out.println("\t❌❌ STOP POR GESTION ALCANZADO ❌❌");
+            System.out.println("\t❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌\n");
+        }else{
+            System.out.println("\n\t✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅");
+            System.out.println("\t✅✅ ESTRATEGIA 100% EFECTIVA ✅✅");
+            System.out.println("\t✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅\n");
+        }
+
+        System.out.println("⚡ Cantidad de Cuentas Quemadas -> " + cantCuentasQuedamas);
+
     }
 
     public static void header(){
