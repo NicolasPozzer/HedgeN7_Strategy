9853e4157dc639febb171350bdd94060099d4609
NicolasPozzer <pozzernico@gmail.com>
2024-12-23 18:59:21 -0300

    Fix: aspect

--- src/main/java/com/example/demo/GUIversion.java
+++ src/main/java/com/example/demo/GUIversion.java
@@ -31,14 +31,18 @@ public class GUIversion extends Application {
         lineChart.setTitle("Evolución del Capital por Cuenta");
 
         // Configuración inicial
-        double capitalInicial = 100;
-        int cantidadDeCuentas = 100; // Usaremos menos cuentas para mejorar la visibilidad
-        int cantidadDeTrades = 2500;
+        int tamanioLineas = 100;
+        double capitalInicial = 500;
+        int cantidadDeCuentas = 10; // Usaremos menos cuentas para mejorar la visibilidad
+        int cantidadDeTrades = 500;
         double TP1 = 0.3709;  // TP1 CON 0.50% DE RIESGO ES: 0.18545. para el 1% es 0.3709.
         double TP2 = 2.132;  // TP2 CON 0.50% DE RIESGO ES: 1.0751, TP2 CON 1.00% DE RIESGO ES: 2.132
         double SL = 1;       // Porcentaje de stop loss 0.50% DE RIESGO ES: 0.50, SL CON 1.00% DE RIESGO ES: 1
-        double probTP1 = 23.222; // mi acierto: probabilidadAciertosTP1 = 22.21;
-        double probTP2 = 27.778; // mi acierto: probabilidadAciertosTP2 = 27.75;
+        double probTP1 = 23.222; // mi acierto: probabilidadAciertosTP1 = 23.222;
+        double probTP2 = 27.778; // mi acierto: probabilidadAciertosTP2 = 27.778;
+
+        int cuentasRentables = 0;  // Contador de cuentas rentables
+        double totalCapitalFinal = 0;  // Para medir el rendimiento total
 
         // Agregar las cuentas al gráfico con una actualización más eficiente
         for (int i = 1; i <= cantidadDeCuentas; i++) {
@@ -57,16 +61,34 @@ public class GUIversion extends Application {
                     capitalActual -= (capitalActual * SL / 100);
                 }
 
-                // Actualiza el gráfico con una selección de cada 10 puntos
-                if (j % 10 == 0) {
+                // Actualiza el gráfico con una selección de cada 50 puntos
+                if (j % tamanioLineas == 0) {
                     series.getData().add(new XYChart.Data<>(j, capitalActual));
                 }
             }
+
+            // Verificar si la cuenta fue rentable
+            if (capitalActual > capitalInicial) {
+                cuentasRentables++;
+            }
+            totalCapitalFinal += capitalActual;
+
             lineChart.getData().add(series);
         }
 
+        // Calcular la probabilidad de rentabilidad
+        double probabilidadRentabilidad = (double) cuentasRentables / cantidadDeCuentas * 100;
+        double rendimientoPromedio = totalCapitalFinal / cantidadDeCuentas;
+
+        double porcentajePromedio = (rendimientoPromedio / capitalInicial) * 100;
+        // Mostrar probabilidad de rentabilidad en la consola
+        System.out.println("Probabilidad de rentabilidad: " + probabilidadRentabilidad + "%");
+        System.out.printf("Rendimiento promedio por cuenta en USD: $ %.2f", rendimientoPromedio);
+        System.out.print("\nRendimiento promedio por cuenta en Porcentaje: %");
+        System.out.printf("%.2f\n", porcentajePromedio);
+
         // Crear la escena y mostrarla
-        Scene scene = new Scene(lineChart, 800, 600);
+        Scene scene = new Scene(lineChart, 1600, 800);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/RentabilityCalculator2TPs.java
+++ src/main/java/com/example/demo/RentabilityCalculator2TPs.java
@@ -11,8 +11,8 @@ public class RentabilityCalculator2TPs {
         final double TP1 = 0.3709; // TP1 CON 0.50% DE RIESGO ES: 0.18545. para el 1% es 0.3709.
         final double TP2 = 2.132; // TP2 CON 0.50% DE RIESGO ES: 1.0751, TP2 CON 1.00% DE RIESGO ES: 2.132
         final double SL = 1; // Porcentaje de stop loss 0.50% DE RIESGO ES: 0.50, SL CON 1.00% DE RIESGO ES: 1
-        final double probabilidadAciertosTP1 = 23.222; // mi acierto: probabilidadAciertosTP1 = 22.21;
-        final double probabilidadAciertosTP2 = 27.778;    // mi acierto: probabilidadAciertosTP2 = 27.75;
+        final double probabilidadAciertosTP1 = 23.222; // mi acierto: probabilidadAciertosTP1 = 23.222;
+        final double probabilidadAciertosTP2 = 27.778;    // mi acierto: probabilidadAciertosTP2 = 27.778;
         final double comisiones = 0.0;
         final double stopGestionDeRiesgo = 50; // si el capital inicial llega a un porcentaje menor a este se deja de operar
 
